460f7c6eaa758060ae679e45aa51f32c552b5fe3
Rinyeki <tomassgomezz812@gmail.com>
2025-11-24 21:42:03 -0300

    Update CORS preflight route to regex pattern
    Changed the CORS preflight route from '*' to a regex pattern to ensure all routes are properly handled for OPTIONS requests.

--- src/app.js
+++ src/app.js
@@ -40,7 +40,7 @@ const corsOptions = {
   credentials: true,
 };
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+app.options(/.*/, cors(corsOptions));
 
 app.use(passport.initialize());
 
