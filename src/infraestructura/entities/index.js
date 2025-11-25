2af44d45ced5880727e98b1255b842ff9e5a2ba2
Rinyeki <tomassgomezz812@gmail.com>
2025-11-24 21:40:28 -0300

    Prevent duplicate PymeEntity association setup
    Added a check to ensure the 'usuario' association on PymeEntity is only created if it does not already exist, preventing potential errors from repeated association setup.

--- src/infraestructura/entities/index.js
+++ src/infraestructura/entities/index.js
@@ -2,11 +2,14 @@ const { UsuarioEntity } = require('./UsuarioEntity.js');
 const { PymeEntity } = require('./PymeEntity.js');
 
 function setupAssociations() {
-  PymeEntity.belongsTo(UsuarioEntity, {
-    foreignKey: 'rut_chileno',
-    targetKey: 'rut_chileno',
-    as: 'usuario',
-  });
+  const assocExists = PymeEntity.associations && PymeEntity.associations.usuario
+  if (!assocExists) {
+    PymeEntity.belongsTo(UsuarioEntity, {
+      foreignKey: 'rut_chileno',
+      targetKey: 'rut_chileno',
+      as: 'usuario',
+    });
+  }
 }
 
 module.exports = { UsuarioEntity, PymeEntity, setupAssociations };
